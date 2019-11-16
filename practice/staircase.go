13ba75eb9dc25fbe3262aa3546d0afb180137a88
TaToonz <peerapath.t@gmail.com>
2019-11-16 13:31:21 +0700

    add url of staircase problem

--- practice/staircase.go
+++ practice/staircase.go
@@ -5,7 +5,8 @@ import (
 	"strings"
 )
 
-// Staircase prints a staircase of size  n
+// Staircase prints a staircase of size n
+// URL: https://www.hackerrank.com/challenges/staircase/problem
 func Staircase(n int32) {
 	for i := int32(1); i <= n; i++ {
 		fmt.Print(strings.Repeat(" ", int(n-i)), strings.Repeat("#", int(i)))
